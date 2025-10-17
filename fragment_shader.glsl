dadbe987741cddd7cd34fa1c8696945c2b1fb086
hellokit123456 <tanishknath@gmail.com>
2025-10-17 14:12:05 +0530

    Update fragment_shader.glsl

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,6 +1,8 @@
 #version 330 core
 out vec4 FragColor;
-
+in vec4 frag_color;
+out vec4 FragColor
 void main() {
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0); // solid white
+    FragColor = color;
+
 }
